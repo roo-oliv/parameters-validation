e991979e7b3f4e8cb2947c7031360730cb69fe26
Rodrigo Oliveira <allrod5@hotmail.com>
2019-06-10 16:35:25 -0300

    Version 1.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras = {
 
 setup(
     name='parameters-validation',
-    version='1.1.1',
+    version='1.1.2',
     packages=['parameters_validation'],
     url='https://github.com/allrod5/parameters-validation',
     license='MIT',
